764159acc54db9998ef2717e511a25b0868f5e7b
sachin <sachinmanojkr@gmail.com>
2025-08-01 17:14:05 +0530

    create directory structure

--- extension.js
+++ extension.js
@@ -2,35 +2,27 @@
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+// Import the commands from your new file. The './' is important.
+const myCommands = require('./funcs/commands.js');
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vs-evil" is now active!');
+    // This line of code will only be executed once when your extension is activated
+    console.log('Congratulations, your extension "vs-evil" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('vs-evil.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+    // Register the command using the imported function from commands.js
+    const disposable = vscode.commands.registerCommand('vs-evil.helloWorld', myCommands.showHelloWorld);
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vs-evil!');
-	});
-
-	context.subscriptions.push(disposable);
+    context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
-}
+    activate,
+    deactivate
+};
---
+++ funcs/commands.js
@@ -0,0 +1,16 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require('vscode');
+
+/**
+ * A command that shows an information message.
+ */
+function showHelloWorld() {
+    // The code you place here will be executed every time your command is executed
+    vscode.window.showInformationMessage('Hello World from a funcs  function!');
+}
+
+// Export the function so it can be used in other files
+module.exports = {
+    showHelloWorld
+};
